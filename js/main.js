54eeebb0937e39e29cb0dedde12e9bbcb0bc9cae
Jessé Júnior <jessejuniormaciel@hotmail.com>
2022-02-04 15:51:08 -0300

    otimizando código referente ao botão voltar ao topo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
+const botaoTopo = document.querySelector(".botao-voltar");
+
 function handleScroll() {
-    const botaoTopo = document.querySelector(".botao-voltar");
     if (window.scrollY > 500) {
         botaoTopo.classList.add("botao-voltar-ativo");
     } else {
         botaoTopo.classList.remove("botao-voltar-ativo");
     }
-
 }
 
 window.addEventListener('scroll', handleScroll)
